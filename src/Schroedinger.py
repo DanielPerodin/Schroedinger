dc3d3853947bb5277a3c862f9cd661471bbb9a1a
UnoriginalUser65 <161094321+UnoriginalUser65@users.noreply.github.com>
2024-03-01 17:36:25 -0500

    made a list of commodities for the user to choose from

--- src/Schroedinger.py
+++ src/Schroedinger.py
@@ -1,3 +1,11 @@
 from Scraper import *
 
+commodity_list = ['cocoa'] 
+print(commodity_list)
 commodity_input = input('Which commodity? ')
+for commodity in commodity_list:
+    if commodity_list == commodity_input.lower:
+        input("What is the stock ticker of the contract?")
+    else:
+        print("Commodity not found, try another commodity.")
+
