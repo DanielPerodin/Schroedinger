8141208e46c3943791fb0e20986e164ff194e4eb
Daniel Perodin <perodindaniel@gmail.com>
2024-03-01 18:33:33 -0500

    Edited the constructor, imported CP to Schroedinger, created Vance object.

--- src/Schroedinger.py
+++ src/Schroedinger.py
@@ -1,10 +1,13 @@
-from Scraper import *
+from Commodity_Predictor import *
 
 commodity_list = ['cocoa'] 
 print(commodity_list)
 commodity_input = input('Which commodity? ')
+ticker = ''
 for commodity in commodity_list:
     if commodity == commodity_input.lower():
        ticker = input("What is the stock ticker of the contract? ")
     else:
         print("Commodity not found, try another commodity.")
+
+Vance = CommodityPredictor(commodity_input, ticker)
