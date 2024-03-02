90929aaedbf99eb26357d769cb7c1481cff73ca2
Daniel Perodin <perodindaniel@gmail.com>
2024-03-01 19:05:23 -0500

    Made a method to run the code that tells scraper which url to check.

--- src/Commodity_Predictor.py
+++ src/Commodity_Predictor.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
+from Scraper import *
 
 class CommodityPredictor:
     def __init__(self, commodity_input, ticker):
         self.commodity_input = commodity_input
         self.ticker = ticker
-
+    def run(self):
+        Daniel = Scraper('https://www.barchart.com/futures/quotes/CC*0/futures-prices')
