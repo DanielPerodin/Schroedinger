21b7e71f58581669638abdafc9921e862791966c
Sunny You <sunny.jiang.you@gmail.com>
2024-03-18 16:05:28 -0400

    First data formatted for ML model.

--- src/Model.py
+++ src/Model.py
@@ -1,5 +1,7 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
 
 class Model:
     def __init__(self, train_inputs, train_output, real_inputs, real_output):
@@ -8,7 +10,7 @@ class Model:
         self.real_inputs = real_inputs
         self.real_output = real_output
     def train(self):
-        self.model = LinearRegression().fit(self.train_inputs, self.train_output)
+        pass
     def predict(self):
         self.predicted_output = self.model.predict(self.real_inputs)
         return self.predicted_output
